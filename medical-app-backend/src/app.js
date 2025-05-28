740146143f1a214c3baa3485c5f6c46daca0e0e6
Letícia França <leticia@MacBook-Air-de-Leticia.local>
2025-05-28 12:43:38 -0300

    Adicionando cors

--- medical-app-backend/src/app.js
+++ medical-app-backend/src/app.js
@@ -1,14 +1,18 @@
 import express from "express";
-import db from "./config/dbConnect.js"
-import routes from "./routes/index.js"
+import cors from "cors";
+import db from "./config/dbConnect.js";
+import routes from "./routes/index.js";
 
-db.on("error", console.log.bind(console, 'Erro de conexão'))
+db.on("error", console.log.bind(console, 'Erro de conexão'));
 db.once("open", () => {
-  console.log('conexão com o banco feita com sucesso')
-})
+  console.log('conexão com o banco feita com sucesso');
+});
 
 const app = express();
-app.use(express.json())
+
+app.use(cors());
+app.use(express.json());
+
 routes(app);
 
-export default app
+export default app;
